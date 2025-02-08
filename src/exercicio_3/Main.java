aeb35ca0d2982c1611e3b09b5574e93d926c23ea
bruno <brunomalaquias012345@gmail.com>
2025-02-08 10:27:29 -0300

    feat: criação da lógica e finalização do exercicio_3

--- src/exercicio_3/Main.java
+++ src/exercicio_3/Main.java
@@ -1,4 +1,12 @@
 package exercicio_3;
 
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                    System.out.println(i);
+            }
+        }
+    }
 }
+
