c3e22bc9916e6f31fca86cf821d1bcbab766a866
bruno <brunomalaquias012345@gmail.com>
2025-02-08 10:05:51 -0300

    feat: criação da lógica da classe Funcionários

--- src/exercicio_1/funcionarios/Funcionarios.java
+++ src/exercicio_1/funcionarios/Funcionarios.java
@@ -1,4 +1,17 @@
 package exercicio_1.funcionarios;
 
 public class Funcionarios {
+    private int id;
+    private String name;
+    private Double salario;
+
+    public Funcionarios(int id, String name, Double salario) {
+        this.id = id;
+        this.name = name;
+        this.salario = salario;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
 }
