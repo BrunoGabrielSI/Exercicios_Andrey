6d8f13f9ac24443ba2d992003722483abf04146b
bruno <brunomalaquias012345@gmail.com>
2025-02-08 10:37:01 -0300

    feat: criação da lógica e finalização do exercicio_4

--- src/exercicio_4/Main.java
+++ src/exercicio_4/Main.java
@@ -1,4 +1,17 @@
 package exercicio_4;
 
 public class Main {
+    public static void main(String[] args) {
+        int x = 13;
+
+        while (x != 1) {
+            System.out.print(x + " -> ");
+            if (x % 2 == 0) {
+                x = x / 2;
+            } else {
+                x = 3 * x + 1;
+            }
+        }
+        System.out.println(x);
+    }
 }
